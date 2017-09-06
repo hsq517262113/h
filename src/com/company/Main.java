223c29863d2eec8dd2d4f3900a6ba62defc9203c
hsq517262113 <517262113@qq.com>
2017-09-06 09:06:21 +0800

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 1;i<10;i++)
+        {
+           System.out.print(i);
+           System.out.print(i+1);
+
+        }
+    }
+}
